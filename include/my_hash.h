13dd49be1f58aa2bab4861e8ef48fb9883c60dc8
Ryan Jones <jonesry@eos22.cis.gvsu.edu>
2018-03-14 14:15:06 -0400

    Initial setup

---
+++ include/my_hash.h
@@ -0,0 +1,22 @@
+template <typename T>
+
+class my_hash {
+
+public:
+
+    my_hash (std::function<int()> hash_function) {
+
+    }
+
+    boolean insert (const Superhero & s) {
+
+    }
+
+
+    Superhero & get (const std::string name) {
+
+    }
+
+private:
+    hash
+}
---
+++ include/superhero.h
@@ -0,0 +1,20 @@
+#include <string>
+
+class superhero {
+public:
+    //TODO
+
+private:
+    int page_id;
+    std::string name;
+    std::string urlslug;
+    std::string id;
+    std::string alignment;
+    char eye_color;
+    char hair_color;
+    std::string gsm;
+    boolean alive;
+    int appearances;
+    std::string first_appearance;
+    int year;
+}
