c67271f27c254d8089699aba521216d7aedc44cd
MintRamney <ryanthechristopherjones@gmail.com>
2018-03-20 02:08:26 -0400

    Basic skeleton of header files

--- include/my_hash.h
+++ include/my_hash.h
@@ -1,22 +1,25 @@
+#import <vector.h>
+
 template <typename T>
 
 class my_hash {
 
 public:
-
     my_hash (std::function<int()> hash_function) {
-
+        this.hash_function = hash_function;
+        data = vector(17011);
     }
 
     boolean insert (const Superhero & s) {
-
+        
     }
 
 
     Superhero & get (const std::string name) {
-
+        
     }
 
 private:
-    hash
+    int *(*hash_function)(int *);
+    vector<vector<T>> data;
 }
