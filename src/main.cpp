de0cfda7183270fddf8e1ec4ad04ee34dfa8e480
MintRamney <ryanthechristopherjones@gmail.com>
2018-03-20 17:42:09 -0400

    syntax

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,9 @@ int hash3 (std::string s) {
     }
 
 int main(int argc, char * argv[]) {
-    io::CSVReader<12, trim_chars<' '>, double_quote_escape<',', "\""> > in("marvel-wikia-data.csv");
+    io::CSVReader<12, io::trim_chars<' '>, io::double_quote_escape<',', "\""> > in("marvel-wikia-data.csv");
 
-    
-    my_hash<superhero> hm1 = new my_hash<superhero>(hash1);
-    my_hash<superhero> hm2 = new my_hash<superhero>(hash2);
-    my_hash<superhero> hm3 = new my_hash<superhero>(hash3);
+    my_hash<superhero>(hash1) hm1;
+    my_hash<superhero>(hash2) hm2;
+    my_hash<superhero>(hash3) hm3;
 }
