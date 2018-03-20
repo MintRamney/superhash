e09542dab8003a1ed44f50db0ce59592d0417d71
MintRamney <ryanthechristopherjones@gmail.com>
2018-03-20 17:30:06 -0400

    one more time

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,22 @@
 #include "csv.h" 
 #include "superhero.h"
 
-int main(int argc, char * argv[]) {
-    io::CSVReader<12, trim_chars<' '>, double_quote_escape<',', "\""> > in("marvel-wikia-data.csv");
-
-    int hash1 (std::string s){
+int hash1 (std::string s){
         return 1;
     }
 
-    int hash2 (std::string s) {
+int hash2 (std::string s) {
         return 2;
     }
 
-    int hash3 (std::string s) {
+int hash3 (std::string s) {
         return 3;
     }
+
+int main(int argc, char * argv[]) {
+    io::CSVReader<12, trim_chars<' '>, double_quote_escape<',', "\""> > in("marvel-wikia-data.csv");
+
+    
     my_hash<superhero> hm1 = new my_hash<superhero>(hash1);
     my_hash<superhero> hm2 = new my_hash<superhero>(hash2);
     my_hash<superhero> hm3 = new my_hash<superhero>(hash3);
