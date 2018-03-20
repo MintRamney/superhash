50b1c45ce0693a6e723de476351c64bd50be584e
MintRamney <ryanthechristopherjones@gmail.com>
2018-03-20 02:25:50 -0400

    Fix syntax errors

--- include/my_hash.h
+++ include/my_hash.h
@@ -1,16 +1,18 @@
 #import <vector.h>
+#import <string>
+#import "superhero.h"
 
 template <typename T>
 
 class my_hash {
 
 public:
-    my_hash (std::function<int()> hash_function) {
+    my_hash (std::function< int(std::string) > hash_function) {
         this.hash_function = hash_function;
         data = vector(17011);
     }
 
-    boolean insert (const Superhero & s) {
+    bool insert (const Superhero & s) {
         
     }
 
@@ -22,4 +24,4 @@ public:
 private:
     int *(*hash_function)(int *);
     vector<vector<T>> data;
-}
+};
